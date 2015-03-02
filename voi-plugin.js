563a2b579924d20ced1eb7d2d3910d5df8aa23ac
ihbtony <ihbtony@gmail.com>
2015-03-01 23:05:20 -0800

    pagination improvements
    better logging, add auto increments to votes, remove extra console logs

--- voi-plugin.js
+++ voi-plugin.js
@@ -60,9 +60,6 @@
                 $('#' + settings.debate_form.attr('id')).append('<p style="color:red">Please add a title</p>');
                 return false;
             }
-
-           // e.preventDefault();
-           console.log('new debate');
             //var data = $(this).serialize();
             //console.log(data);
             //serialize form data, set create to 1 so we know what to do on hook
@@ -84,7 +81,6 @@
 
                     }).done(function(d) {
 
-                        console.log(d);
                         $('#' + settings.debate_form.attr('id')).html(d);//show returned message
 
                     });
@@ -96,7 +92,6 @@
 	//for debate display
     $.fn.voi = function( options ) {
 
-
     	$this = $(this);
 
     	var settings = $.extend({
@@ -105,9 +100,9 @@
             vote 		: null,
             token 		: null,
         }, options);
-
-
       //user not set
+      user = '';
+
       if(settings.users == null) {
 
         //get the cookie scripte from voi.com so we can start setting cookies. this will be our new unique user id.
@@ -116,25 +111,23 @@
           "http://www.voteoverit.com/js/jscookie.js",function(e) {
 
       var CookieSet = $.cookie('voi'); 
-
       if (CookieSet == null) { //check if voi cookie is set
           var timenow = $.now();
           $.cookie("voi", timenow, { expires: 365, path: '/' }); //set cookie id voi to timestamp and to expire in 1 year
           user = $.cookie('voi');
         } else {
           user = $.cookie('voi');
+          console.log('user c 1 :' + user);
         }
 
       });
-
       } else {
         user = settings.users;
       }
 
+
 setTimeout(function() {
       // Do something after 5 seconds
-
-      console.log('user:' + user);
         $.post('http://www.voteoverit.com/connect/debate_display.php', 
     		{ token : settings.token,
     		  title : settings.url,
@@ -146,14 +139,8 @@ setTimeout(function() {
   $(document).on("click", ".userCommentEdit", function() {
   //$(".userCommentEdit").click(function() {
 
-    console.log('user comment edit clicked');
-
-    console.log( $(this).text() );
-var ele = $(this).attr('id').split('-');
+    var ele = $(this).attr('id').split('-');
     if( $(this).text() == 'EDIT') {
-
-      console.log('element id:' + ele);
-      console.log('value text = Edit');
 
       var ele = $(this).attr('id').split('-');
       $( "#pollText_" + ele[2] ).hide();
@@ -164,10 +151,8 @@ var ele = $(this).attr('id').split('-');
 
     } else if($(this).text() == 'SAVE') {
       
-      //update
-      console.log($(this).attr('id'));
       ele = $(this).attr('id').split('-');
-      console.log(ele[0]);
+      issue_id = ele[1];
 
       var $this = $(this);
 
@@ -176,24 +161,21 @@ var ele = $(this).attr('id').split('-');
         id: ele[2],
         Updated: "",
         pollText : $("#comment_" + ele[2]).val(),
-        user : user
+        user : user,
+        issue_id : ele[1]
 
       }).done(function(data) {
 
-        console.log('edit data:' + data);
-        if(data == 'Poll Saved') {
-          $(".addCommentDiv").html('<div class="userCommentDiv" id="userCommentDiv_60" style="display: inline-block;"><div class="debateImageDiv latter" style="margin-top:2px; float:left;"><a href="http://www.voteoverit.com/myprofile/history.php?user=3"><img id="debateImage" src="http://www.voteoverit.com/member/profileImages/3.jpg" style="height:40px;"></a></div><div id="comment_3" style="float:left; width:300px;"><span class="userCommentText" id="pollText_60">' + $("#comment_" + ele[2]).val() + '<font class=""></font></span><textarea type="text" class="addCommentText" id="comment_60" style="display:none; height:78px; min-width:320px; max-width:280px; margin:0px 6px;">' + $("#comment_" + ele[2]).val() + '</textarea></div><div style="float:right"><button class="userCommentEdit" id="comment-3-60">EDIT</button><br><button class="userCommentRemove" id="remove-3-60" style="">REMOVE</button></div><br></div>');
+        var return_data = $.parseJSON(data);
+        PollID = return_data.PollID; //poll id from insert returned
+        
+        $(".addCommentDiv").html('<div class="userCommentDiv" id="userCommentDiv_60" style="display: inline-block;"><div class="debateImageDiv latter" style="margin-top:2px; float:left;"><a href="http://www.voteoverit.com/myprofile/history.php?user=3"><img id="debateImage" src="http://www.voteoverit.com/member/profileImages/3.jpg" style="height:40px;"></a></div><div id="comment_' + issue_id + '" style="float:left; width:300px;"><span class="userCommentText" id="pollText_60">' + $("#comment_" + ele[2]).val() + '<font class=""></font></span><textarea type="text" class="addCommentText" id="comment_' + PollID + '" style="display:none; height:78px; min-width:300px; max-width:300px; margin:0px 6px;">' + $("#comment_" + ele[2]).val() + '</textarea></div><div style="float:right;"><button class="userCommentEdit" id="comment-' + issue_id + '-' + PollID + '">EDIT</button><br><button class="userCommentRemove" id="remove-' + issue_id + '-' + PollID + '">REMOVE</button></div><br></div>');
             //$("#comment_" + ele[2]).val() );
           $(".addCommentDiv").show();
           $('.userCommentText').html( $("#comment_" + ele[2]).val() );
-          $('.userCommentText').show();
-        } 
-                                              
-
+          $('.userCommentText').show();                          
 
         $("#comment_" + ele[2]).hide(); 
-        console.log($("#comment_" + ele[2]).val());
-
         //redisplay polltext and put in the comment contents
         $( "#pollText_" + ele[2] ).html( $("#comment_" + ele[2]).val() + " (SAVED)");
         $( "#pollText_" + ele[2] ).show();
@@ -215,16 +197,13 @@ var ele = $(this).attr('id').split('-');
 
         id: ele[2],
         Updated: "",
-        pollText : ''
+        pollText : '',
+        user: user,
+        remove: 1
 
       }).done(function(data) {
 
-        console.log('data:' + data);
-        $("#userCommentDiv_" + ele[2]).remove();
-
-        $(".addCommentDiv").html('<div class="userCommentDiv" id="userCommentDiv_60" style="display: inline-block;"><div class="debateImageDiv latter" style="margin-top:2px; float:left;"><a href="http://www.voteoverit.com/myprofile/history.php?user=3"><img id="debateImage" src="http://www.voteoverit.com/member/profileImages/3.jpg" style="height:40px;"></a></div><div id="comment_3" style="float:left; width:300px;"><span class="userCommentText" id="pollText_60"><font class=""></font></span><textarea type="text" class="addCommentText" id="comment_60" style="display:none; height:78px; min-width:320px; max-width:280px; margin:0px 6px;"></textarea></div><div style="float:right"><button class="userCommentEdit" id="comment-3-60">EDIT</button><br><button class="userCommentRemove" id="remove-3-60" style="">REMOVE</button></div><br></div>');
-
-          console.log('parent append!');
+        reload_vote_display();
 
       }); 
   });
@@ -239,15 +218,11 @@ var ele = $(this).attr('id').split('-');
 
   $(document).on("click", ".addCommentButton", function() {
 
-    console.log('add comment clicked');
-
-      console.log($(this).attr('id'));
       var ele = $(this).attr('id').split('-');
-      console.log(ele[0]);
 
       issue_id = ele[1];
       vote = ele[2];
-      $this = $(this);
+
       console.log(issue_id + "\r\n" + 'vote:' + vote + "\r\n" + user + "\r\n" + $("#comment_" + issue_id).val());
 
     $.post("http://www.voteoverit.com/API/polls/add", { 
@@ -259,13 +234,13 @@ var ele = $(this).attr('id').split('-');
           vote: vote 
 
       }).done(function(data) { 
-        console.log( ' return from add comment:' + data);
+
         var return_data = $.parseJSON(data);
         PollID = return_data.PollID; //poll id from insert returned
         if(return_data.message != 'Problem saving opinion') {
           console.log("comment saved");
 
-          $("#bar_" + issue_id).append("<div class='userCommentDiv' id='userCommentDiv_" + PollID + "' style='display:inline-block;'><div class='debateImageDiv latter' style='margin-top:2px; float:left;'><img id='debateImage' src='http://www.voteoverit.com/member/profileImages/6307.jpg' style='height:40px;'/></div><div id='comment_" + issue_id + "' style='float:left; width:340px;'><span class='userCommentText' id=''>" + $('#comment_' + issue_id).val() + "<font class=''></font> -  <font class=''> (ADDED) </font></span><textarea type='text' class='addCommentText' id='comment_" + PollID + "' style='display:none; height:78px; min-width:320px; max-width:320px; margin:0px 6px;'>" + $('#comment_' + issue_id).val() + "</textarea></div><div style='float:right;'><button class='userCommentEdit' id='comment-" + issue_id + "-" + PollID + "'>EDIT</button><br><button class='userCommentRemove' id='remove-" + issue_id + "-" + PollID + "' style=''>REMOVE</button></div><br></div>");
+          $("#bar_" + issue_id).append("<div class='userCommentDiv' id='userCommentDiv_" + PollID + "' style='display:inline-block;'><div class='debateImageDiv latter' style='margin-top:2px; float:left;'><img id='debateImage' src='http://www.voteoverit.com/member/profileImages/6307.jpg' style='height:40px;'/></div><div id='comment_" + issue_id + "' style='float:left;'><span class='userCommentText' id=''>" + $('#comment_' + issue_id).val() + "<font class=''></font> -  <font class=''> (ADDED) </font></span><textarea type='text' class='addCommentText' id='comment_" + PollID + "' style='display:none; height:78px; min-width:300px; max-width:300px; margin:0px 6px;'>" + $('#comment_' + issue_id).val() + "</textarea></div><div style='float:right;'><button class='userCommentEdit' id='comment-" + issue_id + "-" + PollID + "'>EDIT</button><br><button class='userCommentRemove' id='remove-" + issue_id + "-" + PollID + "' style=''>REMOVE</button></div><br></div>");
 
 
             $(".addCommentDiv").remove(); //remove add comment div and start using UserCommentDiv since they now have an existing comment in place
@@ -276,9 +251,11 @@ var ele = $(this).attr('id').split('-');
 
     });
 
-    $(document).on("click", ".addCommentCancel", function() {
-        $(this).parent().hide();
-    });
+  $(document).on("click", ".addCommentCancel", function() {
+    console.log('hide addCommentDiv');
+    $(".addCommentText").val('');
+    $(".addCommentDiv").hide();
+  });
 
   $(document).on("click", ".userComment", function() {
     $(this).next('.addCommentDiv').slideToggle(300, 'linear');
@@ -288,21 +265,25 @@ var ele = $(this).attr('id').split('-');
 
   $(document).on('click', '.voi-plus', function() {
 
-  		console.log('add comment vote');
-
       var id = $(this).attr("id").split("_")
       var elementID = id[1];
       var $this = $(this);
-      console.log(elementID + 'comment positive');
+
       $.post("http://www.voteoverit.com/API/Polls/vote/1/" + elementID + "/" + user, 
       {   user: user,
           id: elementID,
           vote: 1 }).done(function(data) {
 
-                console.log(data);
                 if ((data) == "Already voted on this opinion!") {
                     console.log('already voted');
                 } else {
+
+            var current_count = parseInt( $this.next('span.commentStat').html().replace('+', '') );
+
+            var new_count = current_count + 1;
+
+            $this.next("span.commentStat").html('+ ' + new_count);
+
             $this.addClass("voi-plus-fill");
             $this.removeClass("voi-plus");
             $("#messageMessage").html("<span class='influence'>+1pt</span> For Comment Vote<br>");
@@ -312,9 +293,7 @@ var ele = $(this).attr('id').split('-');
 
    });  
 
-  function reload_vote_display(voteType) {
-
-  	  	console.log('reload called')
+  function reload_vote_display() {
 
   	$.post('http://www.voteoverit.com/connect/debate_display.php', 
     		{ token : settings.token,
@@ -323,25 +302,6 @@ var ele = $(this).attr('id').split('-');
           user  : user }).done(function(data) {
     		  	$this.html(data);
     });   
-          console.log('after reload');
-
-  	//var total_votes = $("#total_votes").html();
-  	//var max_votes = $("#max_votes").html();
-  	//var pos_votes = $("#yay_percentage").html().replace('%', '');
-  	//var neg_votes = $("#nay_percentage").html().replace('%', '');
-///
-  //	$("#total_votes").html( parseInt(total_votes + 1) );
-
-  	//if(voteType == 'positive') {
-
-	//	$("#yay_percentage").html( (parseInt(total_votes + 1) * ( parseInt(pos_votes/100) ) ).toFixed() );
-
-  	//} else {
-
-  	//	$("#nay_percentage").html( (parseInt(total_votes + 1) * ( parseInt(neg_votes/100) ) ).toFixed() );
-
-
-  	///}
   	
   }
     
@@ -357,8 +317,15 @@ var ele = $(this).attr('id').split('-');
           vote: 0 }).done(function(data) {
         console.log(data);
         if ((data) == "Already voted on this opinion!") {
-            console.log('already voted on comment');
+
         } else {
+            
+            var current_count = parseInt( $this.prev('span.commentStat').html().replace('-', '') );
+
+            var new_count = current_count + 1;
+
+            $this.prev("span.commentStat").html('- ' + new_count);
+
           $this.addClass("voi-minus-fill");
           $this.removeClass("voi-minus");
           $("#messageMessage").html("<span class='influence'>+1pt</span> For Comment Vote<br>");
@@ -376,9 +343,6 @@ var ele = $(this).attr('id').split('-');
       $("#current_vote").val('1');
       $("#current_issue_id").val(id);
 
-
-    console.log('pos button clicked testing');
-
     	$.post('http://www.voteoverit.com/connect/api_hook.php', 
              {token : settings.token,
               title : settings.url,
@@ -387,8 +351,6 @@ var ele = $(this).attr('id').split('-');
 
         if ( data == 'Already voted' || data == 'Vote Not Saved') {
             
-            console.log('already voted on issue');
-
             //we're passing back the login stuff here because we don't have a valid email associated with this user id
         } else if( data.indexOf("<div class='signin'") >= 0) {
 
@@ -396,8 +358,8 @@ var ele = $(this).attr('id').split('-');
           $('.votingArea').prepend(data); ///append the html for them to add an email
 
         } else {
-console.log('saved');
-            reload_vote_display('positive');
+
+            reload_vote_display();
                   //$("#message" ).show(1000, "swing", callbackMessage );  
             $("#bar_" + id).show();
             $("#neg_button_" + id).remove();
@@ -417,7 +379,6 @@ console.log('saved');
         $("#current_vote").val('0');
         $("#current_issue_id").val(id);
 
-        console.log('neg buttonc licked');
     $.post('http://www.voteoverit.com/connect/api_hook.php', 
              {token : settings.token,
               title : settings.url,
@@ -426,8 +387,6 @@ console.log('saved');
 
         if ( data == 'Already voted' || data == 'Vote Not Saved') {
             
-                console.log('already voted');
-
             //we're passing back the login stuff here because we don't have a valid email associated with this user id
         } else if( data.indexOf("<div class='signin'") >= 0) {
 
@@ -435,12 +394,12 @@ console.log('saved');
           $('.votingArea').prepend(data);  ///append the html for them to add an email
           
         } else {
-            reload_vote_display('negative');
-        	console.log('saved');
-                 // $("#message").show(1000, "swing", callbackMessage );  
-                  $("#bar_" + id).show();
-                  $("#pos_button_" + id).remove();
-                  $ele.css({"margin-top" : "11.5px", "width" : "100%", "text-align" : "center"});
+
+            reload_vote_display(); 
+            $("#bar_" + id).show();
+            $("#pos_button_" + id).remove();
+            $ele.css({"margin-top" : "11.5px", "width" : "100%", "text-align" : "center"});
+
           }
     }); 
 
@@ -454,7 +413,7 @@ console.log('saved');
     if ( $( $("#updates") ).prop( "checked" ) ) {
       var updates = 1;
     }
-    console.log(' save email clicked');
+
     $.post('http://www.voteoverit.com/connect/api_hook.php', 
         { update : updates,
           user_id : $("#user_id").val(),
@@ -462,20 +421,17 @@ console.log('saved');
           vote : $("#current_vote").val(),
           issue_id : $("#current_issue_id").val()
           }).done(function(data) {
-      console.log('return');
-      console.log(data);
+
       $(".signin").hide();
       $(".votebuttons").show();
       $("#bar_" + id).show();
 
-      console.log('voted');
       if($("#current_vote").val() == 1) {
-      	console.log('pos');
       	$("#neg_button_" + $("#current_issue_id").val()).remove();
       	$("#pos_button_" + $("#current_issue_id").val()).css({"margin-top" : "11.5px", "width" : "100%", "text-align" : "center"});
 
       } else {
-      	console.log('neg');
+
       	$("#pos_button_" + $("#current_issue_id").val()).remove();
       	$("#neg_button_" + $("#current_issue_id").val()).css({"margin-top" : "11.5px", "width" : "100%", "text-align" : "center"});
 
@@ -485,24 +441,12 @@ console.log('saved');
 
   });
 
-
-  //for login info if user is not logged in. logged in is indicated by having a valid email
-
   $(document).on('click', '#toggleView', function() {
     $(".voi-angle-right").toggleClass(function(){
       return "voi-angle-down";
     });
 
   });
-
- // $(".addLinkDiv").hide();
- // $(".linkAdd").click(function() {
- //   $(this).prev(".addLinkDiv").slideToggle(300, "linear");
-  //  $(this).html("+ DONE");  // USE THIS TO ADD A LINK TO THE DEBATE // NEED TO RESET THE FORM AFTER
- //   $(this).next(".linkCancel").html("x CANCEL");
-//  });
-
- // $(".addCommentDiv").hide();
 
   $(document).on('click', '.addComment', function() {
     $(this).next(".addCommentDiv").slideToggle(300, "linear");
@@ -513,11 +457,6 @@ console.log('saved');
   $(document).on('click', '.userComment', function() {
     $(this).next(".userCommentDiv").slideToggle(300, "linear");
   });
-
-  // $('.CollapsibleSunlight').hide();
-
-  //$('.CollapsibleLinks').hide();
- // $('.CollapsibleComments').hide();
 
  $(document).on('click', '.CollapsibleTab', function() {
     $(this).next('.CollapsibleComments').slideToggle(300, 'linear');
@@ -546,7 +485,7 @@ console.log('saved');
 
  		new_page_num = page_num - 1;
  		$("#page_number").val(parseInt(new_page_num));
-
+    $("#page_num_display").html(new_page_num);
  		console.log('page number called'+ new_page_num);
  		$.post('http://www.voteoverit.com/connect/debate_display.php', 
  			{page:new_page_num, 
@@ -573,14 +512,14 @@ console.log('saved');
  	new_page_num = page_num + 1;
 
  	$("#page_number").val(parseInt(new_page_num));
+  $("#page_num_display").html(new_page_num);
+
 console.log('page:' + new_page_num);
  		$.post('http://www.voteoverit.com/connect/debate_display.php', 
  			{page:new_page_num, 
  		     issue_id : $(this).data('issueid'), 
  		     user: user
  		    }).done(function(data){
-
- 			console.log(data);
 
  			if(data == 'No Comments') {
  				$("#page_number").val(page_num);
@@ -601,9 +540,7 @@ console.log('page:' + new_page_num);
  		issue_id: $(this).data('issueid'),
  		from_url: window.location.href
 
- 	}).done(function(data) {
- 		console.log(data);
- 	});
+ 	})
 
  });
 
